d3d049d16ad5f200910f44f56cdb23a8b83f0cce
Aman Rangapur <amanrangapur@gmail.com>
2022-12-15 17:30:28 -0600

    iris

--- iris.py
+++ iris.py
@@ -33,22 +33,38 @@ class Iris(nn.Module):
     def __init__(self,input_dim):
         super(Iris, self).__init__()
         self.fc1 = nn.Linear(input_dim, 64)
-        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+        self.bn = nn.BatchNorm1d(64)
+        self.softmax = nn.Softmax()
+        self.dropout = nn.Dropout()
         self.fc2 = nn.Linear(64, 32)
+        self.bn1 = nn.BatchNorm1d(32)
         self.fc3 = nn.Linear(32, 16)
-        self.fc4 = nn.Linear(16, 4)
-        self.fc5 = nn.Linear(4, 3)
+        self.bn2 = nn.BatchNorm1d(16)
+        self.fc4 = nn.Linear(16, 3)
+        self.bn3 = nn.BatchNorm1d(3)
+
 
     def forward(self, x):
         x = self.fc1(x)
-        x = self.relu(x)
+        x = self.bn(x)
+        x = self.sigmoid(x)
+        x =self.dropout(x)
+
         x = self.fc2(x)
-        x = self.relu(x)
+        x = self.bn1(x)
+        x = self.sigmoid(x)
+        x =self.dropout(x)
+
         x = self.fc3(x)
-        x = self.relu(x)
+        x = self.bn2(x)
+        x = self.sigmoid(x)
+        x =self.dropout(x)
+
         x = self.fc4(x)
-        x = self.relu(x)
-        x = self.fc5(x)
+        x = self.bn3(x)
+        x = self.softmax(x)
+
         return x
 
 model = Iris(X_train.shape[1]).to(device)
