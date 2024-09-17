2fa3b2ed24b7b3a688270c375c1643d116123ac7
aman-17 <amanrangapur@gmail.com>
2024-09-17 07:02:54 -0500

    added preprocessing for llm from scratch

---
+++ tokenization/tokenize_and_embed.py
@@ -0,0 +1,56 @@
+import re
+import tiktoken
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+class GPTDatasetV1(Dataset):
+    def __init__(self, txt, tokenizer, max_length, stride):
+        self.tokenizer = tokenizer
+        self.input_ids = []
+        self.target_ids = []
+        token_ids = tokenizer.encode(txt) #A
+        for i in range(0, len(token_ids) - max_length, stride): #B
+            input_chunk = token_ids[i:i + max_length]
+            target_chunk = token_ids[i + 1: i + max_length + 1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+
+    def __len__(self): #C
+        return len(self.input_ids)
+    
+    def __getitem__(self, idx): #D
+        return self.input_ids[idx], self.target_ids[idx]
+    
+
+def create_dataloader_v1(txt, batch_size=4,
+        max_length=256, stride=128, shuffle=True, drop_last=True):
+    tokenizer = tiktoken.get_encoding("gpt2") #A
+    dataset = GPTDatasetV1(txt, tokenizer, max_length, stride) #B
+    dataloader = DataLoader(
+        dataset, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last)
+    return dataloader
+
+with open("tokenization/the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+dataloader = create_dataloader_v1(
+    raw_text, batch_size=1, max_length=4, stride=1, shuffle=False)
+
+data_iter = iter(dataloader) #A
+first_batch = next(data_iter)
+print(first_batch[0][0])
+
+# dataloader = create_dataloader_v1(raw_text, batch_size=8, max_length=4, stride=1, shuffle=False)
+# data_iter = iter(dataloader)
+# inputs, targets = next(data_iter)
+# print("Inputs:\n", inputs)
+# print("\nTargets:\n", targets)
+
+input_ids = torch.tensor([2, 3, 5, 1])
+vocab_size = 50257
+output_dim = 256
+torch.manual_seed(123)
+embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
+# print(embedding_layer.weight)
+# print(embedding_layer(torch.tensor(first_batch[0][0])))
+token_embeddings = embedding_layer(input_ids)
+print(token_embeddings.shape)
---
+++ tokenization/tokenize_from_scratch.py
@@ -0,0 +1,40 @@
+import re
+
+with open("tokenization/the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+
+preprocessed = re.split(r'([,.?_!"()\']|--|\s)', raw_text)
+preprocessed = [item.strip() for item in preprocessed if item.strip()]
+
+all_words = sorted(list(set(preprocessed)))
+all_words.extend(["<|endoftext|>", "<|unk|>"])
+vocab = {token:integer for integer,token in enumerate(all_words)}
+
+class SimpleTokenizerV1:
+    def __init__(self, vocab):
+        self.str_to_int = vocab #A
+        self.int_to_str = {i:s for s,i in vocab.items()} #B
+    def encode(self, text): #C
+        preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        preprocessed = [item if item in self.str_to_int  #A
+                        else "<|unk|>" for item in preprocessed]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+    def decode(self, ids): #D
+        text = " ".join([self.int_to_str[i] for i in ids])
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text) #E
+        return text
+    
+
+tokenizer = SimpleTokenizerV1(vocab)
+text = """"It's the last he painted, you know," Mrs. Gisburn said pard"""
+ids = tokenizer.encode(text)
+# print(ids)
+# print(tokenizer.decode(ids))
+
+text1 = "Hello, do you like tea?"
+text2 = "In the sunlit terraces of the palace."
+text = " <|endoftext|> ".join((text1, text2))
+print(text)
+print(tokenizer.decode(tokenizer.encode(text)))
